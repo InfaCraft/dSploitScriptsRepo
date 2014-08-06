c3f6dba0f793987ef731a247715b490df17b7643
jkush321 <jkush321@yahoo.com>
2014-08-06 12:19:57 -0400

    A test script

---
+++ scripts/testscript.js
@@ -0,0 +1,9 @@
+<script>
+	// Author: jkush321
+	// Name: Test Script
+	// Proper Name: testfile
+	// Description: A test script for testing this app. Much work. Such excite.
+
+	alert("This page has been hacked with dSploit!");
+	alert("This script has been downloaded from dSploitScripts!");
+</script>
