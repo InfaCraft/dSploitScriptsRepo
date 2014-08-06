edfff7beab4dd4bb45a7918b6545aefbfa94050e
jkush321 <jkush321@yahoo.com>
2014-08-06 12:34:43 -0400

    ProperName is stupid

--- scripts/testscript.js
+++ scripts/testscript.js
@@ -1,7 +1,6 @@
 <script>
 	// Author: jkush321
 	// Name: Test Script
-	// Proper Name: testfile
 	// Description: A test script for testing this app. Much work. Such excite.
 
 	alert("This page has been hacked with dSploit!");
