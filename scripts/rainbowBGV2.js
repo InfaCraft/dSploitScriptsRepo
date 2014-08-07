b10677fdcd58d2d5a6e827414ef3772ab63a3a7e
jkush321 <jkush321@yahoo.com>
2014-08-07 14:16:13 -0400

    Update rainbow bg

--- scripts/rainbowBGV2.js
+++ scripts/rainbowBGV2.js
@@ -1,19 +1,20 @@
 <script>
 	// Author: jkush321
-	// Name: Rainbow Background
+	// Name: Rainbow Background v2
 	// Description: The background changes colors like a rainbow, so beautiful
 
 	var tick = 0;
 	var rate = 0.1;
-	window.onload=function() {
-		setInterval(function()
-		{
+	window.onload=function(){
+		setInterval(function(){
 			tick=tick + rate;
 			var red = Math.sin(tick) * 127 + 128;
 			var green = Math.sin(tick + 90) * 127 + 128;
 			var blue = Math.sin(tick + 270) * 127 + 128;
-			red=parseInt(red);green=parseInt(green);blue=parseInt(blue);
+			red=parseInt(red);
+			green=parseInt(green);
+			blue=parseInt(blue);
 			document.body.style.backgroundColor="rgb("+red+","+green+","+blue+")";
 		},50);
-	}
+	};
 </script>
