9146b8dae4243215fbae4bf78d469115c9e5dee9
jkush321 <jkush321@yahoo.com>
2014-08-06 23:02:10 -0400

    Fixed rickRollMusic

--- scripts/rickRollMusic.js
+++ scripts/rickRollMusic.js
@@ -2,10 +2,7 @@
 	// Author: jkush321
 	// Name: Rick Roll (Music)
 	// Description: Plays Never Gonna Give You Up via an invisible YouTube video
-</script>
-<iframe width="0" height="0" src="//www.youtube.com/embed/6_b7RDuLwcI" frameborder="0" allowfullscreen></iframe>
-<script>
-	// We include an extra set of script tags because
-	// the file should start with <script
-	// and end with /script>
+	window.onload=function(){
+		document.write("<iframe width=\"0\" height=\"0\" src=\"//www.youtube.com/embed/okqEVeNqBhc?autoplay=1\" frameborder=\"0\" allowfullscreen></iframe>");
+	}
 </script>
