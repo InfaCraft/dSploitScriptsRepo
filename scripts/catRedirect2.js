8580d053c55707ae8da908050a8787891322242a
jkush321 <jkush321@yahoo.com>
2014-08-06 22:48:42 -0400

    Revert "Fixed catRedirect AGAIN"
    This reverts commit b548cc27441008bd95133bf2d4ac623adef6b413.

--- scripts/catRedirect2.js
+++ scripts/catRedirect2.js
@@ -4,6 +4,6 @@
 	// Description: Redirects the user to a google image search of "cats"
 
 	window.onload = function(){
-		window.location = "https://google.com/search?tbm=isch&q=cats&gws_rd=ssl";
+		window.location = "https://images.google.com/search?tbm=isch&q=cats&gws_rd=ssl";
 	}
 </script>
