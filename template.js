49a4ca67b2b8d1ba085e7ac290adf9c83b76b94f
jkush321 <jkush321@yahoo.com>
2014-08-06 21:05:40 -0400

    Added template.js

---
+++ template.js
@@ -0,0 +1,34 @@
+<script>
+	// Author: [Your username|Email|Real Name]
+	// Name: [Name of the script]
+	// Description: [Description of the script]
+
+	alert("This is a template!");
+</script>
+
+//---------------------------//
+//      IMPORTANT NOTES      //
+//---------------------------//
+//   All files must begin    //
+//   with <script> and end   //
+//  with </script> or else   //
+//    problems may arise.    //
+//---------------------------//
+//   Avoid using ' (single   //
+//    quotes). They will     //
+//    cause issues as well   //
+//---------------------------//
+//  Once you submit a script //
+//   it will be permanently  //
+//   cached and uneditable,  //
+//  so include a version in  //
+//   the name of the file.   //
+//     Ex: myScriptV1.js     //
+//---------------------------//
+//  You can either submit    //
+// a pull request, or email  //
+//      your script to       //
+//dsploitscripts@infacraft.net/
+//---------------------------//
+//---------------------------//
+//---------------------------//
