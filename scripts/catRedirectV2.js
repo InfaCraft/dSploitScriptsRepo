ae9871b94d18d837a974da78832de19ca3766ec5
jkush321 <jkush321@yahoo.com>
2014-08-06 22:48:57 -0400

    Fixed catRedirect for the last time

--- scripts/catRedirectV2.js
+++ scripts/catRedirectV2.js
@@ -1,9 +1,9 @@
 <script>
 	// Author: jkush321
-	// Name: Cat Redirect 2
+	// Name: Cat Redirect v2
 	// Description: Redirects the user to a google image search of "cats"
 
 	window.onload = function(){
-		window.location = "https://images.google.com/search?tbm=isch&q=cats&gws_rd=ssl";
+		window.location = "https://google.com/search?tbm=isch&q=cats&gws_rd=ssl";
 	}
 </script>
